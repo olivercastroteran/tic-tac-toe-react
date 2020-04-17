4664c4164bf0b52c92d1f1d5fdfd421719d15cbd
olivercastroteran <olivercastroteran@gmail.com>
2020-04-16 20:45:58 -0400

    turns functionality implemented

--- src/index.js
+++ src/index.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return <button className="square">{/* TODO */}</button>;
-  }
-}
+const Square = (props) => {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+};
 
-class Board extends React.Component {
+class Board extends Component {
+  state = {
+    squares: Array(9).fill(null),
+    xIsNext: true,
+  };
+
+  handleClick = (i) => {
+    const squares = [...this.state.squares];
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  };
+
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
@@ -39,7 +60,7 @@ class Board extends React.Component {
   }
 }
 
-class Game extends React.Component {
+class Game extends Component {
   render() {
     return (
       <div className="game">
